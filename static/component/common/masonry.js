d6dcd5cb22686e51af4c66dc02bfc3b0f8e28072
john <bingcola@aliyun.com>
2018-06-10 21:51:01 +0800

    dsad

--- static/component/common/masonry.js
+++ static/component/common/masonry.js
@@ -1,49 +1,61 @@
 (function(exports) {
-    function Cmpt(container, option) {
-        this.container = container;
-        this.option = option || {};
-        this.view = {};
+    class Cmpt {
+        constructor(container, option) {
+            this.container = container;
+            this.option = option || {};
+            this.view = {};
 
-        this.store = [];
-        this.cursor = {
-            x: 0,
-            y: 0,
-            col: 0
+            this.store = [];
+            this.cursor = {
+                x: 0,
+                y: 0,
+                col: 0
+            }
+            this.colCursor = []
+            this.aysncAble = true;
         }
-        this.colCursor = []
-        this.aysncAble = true;
-    }
-    Cmpt.prototype = {
-        init: function() {
+        get DEFAULT_OPTION() {
+            return {
+                layout: {
+                    col: Math.floor(this.container.offsetWidth / 300)
+                },
+                item: {
+                    margin: 20
+                },
+                event: {
+                    onScroll: null,
+                    onClick: null,
+                    onItemInsert: null,
+                },
+                aysnc: {
+                    enable: null,
+                    getData: null
+                },
+                plugin: {
+
+                }
+            }
+        }
+        init() {
             this.initOption();
             this.attachEvent();
-        },
-        attachEvent: function() {
+        }
+        attachEvent() {
             var _this = this;
             this.container.onscroll = this.aysnc
-        },
-        disableaysnc: function() {
+        }
+        disableAysnc() {
             _this.aysncAble = false;
-        },
-        initOption: function() {
-            if (!this.option.view.margin) {
-                this.view.margin = 20;
-            } else {
-                this.view.margin = this.option.view.margin;
-            }
-
-            if (!this.option.col) {
-                this.view.col = Math.floor(this.container.offsetWidth / 300)
-            } else {
-                this.view.col = this.option.col;
-            }
+        }
+        initOption() {
+            this.option = $.extend(true, {}, this.DEFAULT_OPTION, this.option);
 
             this.view.uw = this.container.offsetWidth / this.view.col
             this.initCursor();
 
             this.container.classList.add('cp-masonry-ctn')
-        },
-        initCursor: function() {
+        }
+        initCursor() {
             this.cursor = {
                 x: 0,
                 y: 0,
@@ -53,7 +65,7 @@
             for (var i = 0; i < this.view.col; i++) {
                 this.colCursor.push(0)
             }
-        },
+        }
         aysnc() {
             if (_this.option.aysnc && _this.option.aysnc.getData) {
                 if (_this.aysncAble && _this.container.scrollTop + _this.container.offsetHeight >= _this.container.scrollHeight - 10) {
@@ -70,15 +82,15 @@
                     })
                 }
             }
-        },
-        setItemView: function(items) {
+        }
+        setItemView(items) {
             var _this = this;
             this.store = this.store.concat(items);
             items.forEach(function(item) {
                 _this.appendItem(item)
             })
-        },
-        appendItem: function(item) {
+        }
+        appendItem(item) {
             var dom = this.option.createItemDom(item.data);
             var pos = this.getItemPos({
                 h: item.height,
@@ -90,8 +102,8 @@
             dom.style.top = pos.y + 'px'
             dom.classList.add('cp-masonry-item');
             this.container.appendChild(dom)
-        },
-        getMostBottomPos: function() {
+        }
+        getMostBottomPos() {
             var col = 0;
             var bottom = 0;
             for (var i = 0; i < this.colCursor.length; i++) {
@@ -105,8 +117,8 @@
                 }
             }
             return { col: col, x: this.view.uw * col, y: bottom }
-        },
-        getItemPos: function(size) {
+        }
+        getItemPos(size) {
             var itemPos = {
                 x: this.cursor.x + this.view.margin,
                 y: this.cursor.y + this.view.margin,
@@ -116,15 +128,15 @@
             this.colCursor[this.cursor.col] += size.h * this.view.uw / size.w;
             this.cursor = this.getMostBottomPos();
             return itemPos;
-        },
+        }
 
-        clear: function() {
+        clear() {
             this.store = [];
             this.container.innerHTML = '';
-        },
-        destory: function() {
+        }
+        destory() {
 
-        },
+        }
     }
     exports.masonry = Cmpt;
 })(namespace('component'))
