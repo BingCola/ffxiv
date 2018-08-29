49ae5cd91ecd0383cb82e57ff226c92298c22468
john <bingcola@aliyun.com>
2018-08-30 01:06:34 +0800

    aaa

--- app/webpack.config.base.js
+++ app/webpack.config.base.js
@@ -2,34 +2,142 @@
  * Base webpack config used across other specific configs
  */
 
-import path from "path";
+import path from 'path';
 export const chunks = {
-    app: [path.join(__dirname, "./index.js")]
+    app: [path.join(__dirname, './index.js')],
 };
 
 export default {
+    entry: [...chunks],
     module: {
         rules: [
             {
                 test: /(\.jsx|\.js)$/,
-                use: "babel-loader",
-                exclude: [/node_modules/]
+                use: 'babel-loader',
+                exclude: [/node_modules/],
             },
             {
-                test
-            }
-        ]
+                test: /.sass$/,
+                exclude: [path.resolve(__dirname, 'node_modules')],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /.sass$/,
+                exclude: [path.resolve(__dirname, 'node_modules'), path.resolve(__dirname, 'theme')],
+                use: [
+                    {
+                        loader: 'style-loader', // creates style nodes from JS strings
+                    },
+                    {
+                        loader: 'css-loader', // translates CSS into CommonJS
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: '[name]__[local]__[hash:base64:5]',
+                        },
+                    },
+                    {
+                        loader: 'sass-loader', // compiles Less to CSS
+                    },
+                ],
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader',
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: 'application/font-woff',
+                    },
+                },
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: 'application/font-woff',
+                    },
+                },
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: 'application/octet-stream',
+                    },
+                },
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: 'image/svg+xml',
+                    },
+                },
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+                use: 'url-loader',
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true,
+                    },
+                },
+            },
+        ],
     },
     output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "[name].[chunkhash].js",
-        publicPath: "./public"
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].[chunkhash].js',
+        publicPath: './public',
     },
     resolve: {
-        extensions: [".js", ".jsx", ".json", ".css"],
-        modules: [__dirname, "node_modules"],
+        extensions: ['.js', '.jsx', '.json', '.css'],
+        modules: [__dirname, 'node_modules'],
         alias: {
-            common: path.resolve(__dirname, "./app/common")
-        }
-    }
+            common: path.resolve(__dirname, './app/common'),
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'FFXIV幻化回廊',
+            filename: 'app-dev.html',
+            template: 'index.ejs',
+            alwaysWriteToDisk: true,
+            chunks: ['manifest', 'common', 'dev', 'ui', 'app'],
+            chunksSortMode: 'manual',
+            inject: false,
+        }),
+        new HtmlWebpackHarddiskPlugin({
+            outputPath: __dirname,
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'common.[hash].js',
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest',
+            filename: 'manifest.[hash].js',
+            minChunks: Infinity,
+        }),
+    ],
 };
--- app/webpack.config.dev.js
+++ app/webpack.config.dev.js
@@ -1,7 +1,22 @@
-import "webpack";
-import "path";
-import merge from "webpack-merge";
+import 'webpack';
+import 'path';
+import merge from 'webpack-merge';
 
-import base, { chunks } from "./webpack.config.base";
+import base from './webpack.config.base';
+const port = '7777';
+const publicPath = '/public';
 
-export default merge(base, {});
+export default merge(base, {
+    devServer: {
+        port,
+        host: '0.0.0.0',
+        inline: true,
+        proxy,
+        disableHostCheck: true,
+        historyApiFallback: {
+            verbose: true,
+            index: '/app-dev.html',
+        },
+        publicPath,
+    },
+});
