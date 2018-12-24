333859b25f84a78d83ad2d1cbb987e11a1e6b17e
john <bingcola@aliyun.com>
2018-12-25 01:43:01 +0800

    aaa

--- client/src/plugins/theme/model/border.js
+++ client/src/plugins/theme/model/border.js
@@ -6,26 +6,47 @@ const TEMPLATE = {
     <span class="${CLN.part}"></span>
     <span class="${CLN.part}"></span>
     <span class="${CLN.part}"></span>
-    `
+    `,
+    2: `  
+    <svg class="${CLN.svg}" height="60" width="60" xmlns="http://www.w3.org/2000/svg">
+        <rect class="${CLN.rect}" height="60" width="60"/>
+    </svg>`
+};
+
+const createDom = (ctn, option = {}, model) => {
+    let dom = document.createElement('div');
+    dom.className = CLN['ctn'];
+    dom.dataset.themeBorder = true;
+    dom.dataset.themeModel = model;
+    dom.innerHTML = TEMPLATE[model];
+    ctn.dataset.themed = true;
+    return dom;
+};
+
+const destroy = ctn => {
+    $(ctn)
+        .find(`.${CLN.ctn}`)
+        .remove();
 };
 export default {
     1: {
         set: (ctn, option = {}) => {
-            let dom = document.createElement('div');
-            dom.className = CLN['ctn'];
-            dom.dataset.themeBorder = true;
-            dom.dataset.themeModel = 1;
-            dom.innerHTML = TEMPLATE['1'];
+            let dom = createDom(...arguments, 1);
             if (option.orientation) {
                 dom.dataset.orientation = option.orientation;
             }
-            ctn.dataset.themed = true;
             ctn.appendChild(dom);
         },
-        destroy: ctn => {
-            $(ctn)
-                .find(`.${CLN.ctn}`)
-                .remove();
-        }
+        destroy: destroy
+    },
+    2: {
+        set: (ctn, option = {}) => {
+            let dom = createDom(...arguments, 1);
+            if (option.orientation) {
+                dom.dataset.orientation = option.orientation;
+            }
+            ctn.appendChild(dom);
+        },
+        destroy
     }
 };
